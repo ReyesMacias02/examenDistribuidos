b5e434124904531803364d95937bf6862cf59719
jose reyes <jjrm1351828189@gmail.com>
2023-05-21 03:10:30 -0500

    subir

--- client/client.py
+++ client/client.py
@@ -1,1 +1,87 @@
-print('hola')
+import socket
+from tabulate import tabulate
+# Configuración del servidor
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 60080
+BUFFER_SIZE = 1024
+
+# Crear socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def connect_to_server():
+    # Conectar al servidor
+    print('Conectando al servidor {} puerto {}'.format(SERVER_HOST, SERVER_PORT))
+    sock.connect((SERVER_HOST, SERVER_PORT))
+
+def send_request(data):
+    # Enviar solicitud al servidor
+    sock.sendall(data.encode())
+
+def receive_response():
+    # Recibir respuesta del servidor
+    response = sock.recv(BUFFER_SIZE).decode()
+    return response
+
+def close_connection():
+    # Cerrar la conexión
+    sock.close()
+
+def main_menu():
+    # Menú principal
+    print('Bienvenido al sistema de consulta y actualización de saldos')
+    print('1. Consultar información de un cliente')
+    print('2. Agregar saldo a un cliente')
+    print('0. Salir')
+
+def consultar_cliente():
+    # Consultar información de un cliente
+    cedula = input('Ingrese la cédula del cliente: ')
+    accion='consultar'
+    saldo='0'
+    data = cedula + ',' + accion + ',' + saldo
+    send_request(data)
+    response = receive_response()
+
+    if response.startswith("Apellidos:"):
+        # Obtener los datos de la respuesta
+        datos = response.split(':')
+        apellidos = datos[1].strip().split(',')[0].strip()
+        nombres = datos[2].strip().split(',')[0].strip()
+        saldo = datos[3].strip().split(',')[0].strip()
+
+        # Crear la tabla con los datos
+        headers = ['Apellidos', 'Nombres', 'Saldo']
+        data = [[apellidos, nombres, saldo]]
+        table = tabulate(data, headers, tablefmt='grid')
+        print(table)
+    else:
+        print(response)
+def agregar_saldo():
+    # Agregar saldo a un cliente
+    cedula = input('Ingrese la cédula del cliente: ')
+    saldo = input('Ingrese el saldo a agregar: ')
+    accion='actualizar'
+    data = cedula + ',' + accion + ',' + saldo
+    send_request(data)
+    response = receive_response()
+    print('Respuesta del servidor:')
+    print(response)
+
+# Conectar al servidor
+connect_to_server()
+
+while True:
+    main_menu()
+    opcion = input('Seleccione una opción: ')
+
+    if opcion == '1':
+        consultar_cliente()
+    elif opcion == '2':
+        agregar_saldo()
+    elif opcion == '0':
+        break
+    else:
+        print('Opción inválida. Intente nuevamente.')
+
+# Cerrar la conexión
+close_connection()
--- server/server.py
+++ server/server.py
@@ -1,1 +1,106 @@
-print('hola')
+import socket
+import psycopg2
+import threading
+
+# Configuración de la base de datos
+DB_HOST = 'localhost'
+DB_PORT = 5432
+DB_NAME = 'Distribuidos'
+DB_USER = 'postgres'
+DB_PASSWORD = 'admin'
+
+# Configuración del socket
+HOST = '127.0.0.1'
+PORT = 60080
+BUFFER_SIZE = 1024
+
+# Conexión a la base de datos
+conn = psycopg2.connect(host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+cursor = conn.cursor()
+
+# Crear socket TCP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+sock.listen(5)
+
+print('Iniciando el servidor en {} puerto {}'.format(HOST, PORT))
+
+
+def consultar_datos(cedula):
+    # Bloquear la fila del cliente
+    cursor.execute('SELECT * FROM "User" WHERE "User"."idn" = %s FOR UPDATE', (cedula,))
+
+    # Consultar la base de datos
+    cursor.execute('SELECT "User"."Apellidos", "User"."Nombres", "User"."Saldo" FROM "User" WHERE "User"."idn" = %s', (cedula,))
+    result = cursor.fetchone()
+    return result
+
+
+def actualizar_saldo(cedula, nuevo_saldo):
+    # Bloquear la fila del cliente
+    cursor.execute('SELECT * FROM "User" WHERE "User"."idn" = %s FOR UPDATE', (cedula,))
+
+    # Actualizar el saldo en la base de datos
+    cursor.execute('UPDATE "User" SET "Saldo" = %s WHERE "idn" = %s', (nuevo_saldo, cedula))
+    conn.commit()
+
+
+def handle_connection(connection):
+    try:
+        while True:
+            print('-------------------------------')
+            data = connection.recv(BUFFER_SIZE)
+            print('Recibiendo dato: {!r}'.format(data))
+
+            if not data:
+                break
+
+            # Decodificar los datos recibidos
+            cedula, accion, nuevo_saldo = data.decode().split(',')
+
+            if accion == 'consultar':
+                # Consultar los datos de un cliente
+                result = consultar_datos(cedula)
+
+                if result:
+                    apellidos, nombres, saldo = result
+                    response = f"Apellidos: {apellidos}, Nombres: {nombres}, Saldo: {saldo}"
+                else:
+                    response = "No se encontró información para la cédula proporcionada"
+            elif accion == 'actualizar':
+                # Actualizar el saldo de un cliente
+                actualizar_saldo(cedula, nuevo_saldo)
+                response = f"Saldo actualizado para la cédula {cedula}"
+            else:
+                response = "Acción inválida"
+
+            # Enviar respuesta al cliente
+            connection.sendall(response.encode())
+    finally:
+        # Cerrar la conexión
+        connection.close()
+
+
+def accept_connections():
+    while True:
+        # Esperar conexión del cliente
+        print('Esperando conexión de un cliente')
+        connection, client_address = sock.accept()
+
+        print('Conectado desde', client_address)
+
+        # Manejar la conexión en un hilo separado
+        thread = threading.Thread(target=handle_connection, args=(connection,))
+        thread.start()
+
+
+# Iniciar el hilo para aceptar conexiones
+accept_thread = threading.Thread(target=accept_connections)
+accept_thread.start()
+
+# Esperar a que el hilo de aceptar conexiones termine (Ctrl+C para detener el servidor)
+accept_thread.join()
+
+# Cerrar la conexión a la base de datos
+cursor.close()
+conn.close()
